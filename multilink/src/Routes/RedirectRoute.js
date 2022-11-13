b0d0c45377750212c84fd84b7110da13f44dd0ff
Matt Edwards <mattsp.edwards@googlemail.com>
2022-11-13 19:39:13 +0000

    feat: added redirect route to verify JWT tokens

---
+++ multilink/src/Routes/RedirectRoute.js
@@ -0,0 +1,27 @@
+import API from '../Api'
+import React, { useState, useEffect } from 'react'
+import { Navigate } from 'react-router-dom'
+
+export default function RedirectRoute({ children }) {
+    const jwt = localStorage.getItem('access');
+    const [verified, setVerified] = useState(false)
+
+    useEffect(() => {
+        const verify = async () => {
+            await API.post('/auth/jwt/verify/',{
+                'token': jwt
+            }).then((res) => {
+                if (res.status === 200){
+                    setVerified(true)
+                } else {
+                    ['access', 'refresh', 'user'].map((jwtItem) =>
+                        localStorage.removeItem(jwtItem)
+                    )
+                }
+            })
+        }
+        verify()
+    }, [])
+
+    return verified ? children : <Navigate to={`/${localStorage.getItem('user')}`} />
+}
