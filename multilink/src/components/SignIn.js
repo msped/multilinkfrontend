3a19cc35242beffee56f34b97145ac8a1ef78706
Matt Edwards <mattsp.edwards@googlemail.com>
2022-11-21 20:18:36 +0000

    refactor: updated import to apiClient

--- multilink/src/components/SignIn.js
+++ multilink/src/components/SignIn.js
@@ -1,10 +1,6 @@
-import API from '../Api'
-import React from 'react'
-import { 
-    Box,
-    TextField,
-    Button
-} from '@mui/material'
+import apiClient from "../api/apiClient";
+import React from "react";
+import { Box, TextField, Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
 export default function SignIn() {
@@ -13,26 +9,31 @@ export default function SignIn() {
     const handleSubmit = (event) => {
         event.preventDefault();
         const data = new FormData(event.currentTarget);
-        API.post('/auth/jwt/token/', data).then( res => {
-            localStorage.setItem('access', res.data.access)
-            localStorage.setItem('refresh', res.data.refresh)
-            localStorage.setItem('user', res.data.username)
-            API.defaults.headers['Authorization'] =
-                'JWT ' + localStorage.getItem('access')
-            navigate(`/${res.data.username}`)
-        })
+        apiClient.post("/auth/jwt/token/", data).then((res) => {
+            localStorage.setItem("access", res.data.access);
+            localStorage.setItem("refresh", res.data.refresh);
+            localStorage.setItem("user", res.data.username);
+            apiClient.defaults.headers["Authorization"] =
+                "JWT " + localStorage.getItem("access");
+            navigate(`/${res.data.username}`);
+        });
     };
 
-    return (            
+    return (
         <Box
             sx={{
                 mx: 4,
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
             }}
         >
-            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+            <Box
+                component="form"
+                noValidate
+                onSubmit={handleSubmit}
+                sx={{ mt: 1 }}
+            >
                 <TextField
                     margin="normal"
                     required
@@ -63,5 +64,5 @@ export default function SignIn() {
                 </Button>
             </Box>
         </Box>
-    )
+    );
 }
