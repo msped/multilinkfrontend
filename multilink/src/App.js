24ee7c0e0a9bb31e4e0017e9a0e52be69e4c6eec
Matt Edwards <mattsp.edwards@googlemail.com>
2022-11-22 20:24:28 +0000

    refactor: added separate profiles for logged in state

--- multilink/src/App.js
+++ multilink/src/App.js
@@ -6,6 +6,7 @@ import CustomTheme from "./theme";
 
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
+import UserProfile from "./pages/UserProfile";
 
 function App() {
     return (
@@ -14,6 +15,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/me" element={<Profile />} />
+                <Route path="/:username" element={<UserProfile />} />
             </Routes>
         </ThemeProvider>
     );
