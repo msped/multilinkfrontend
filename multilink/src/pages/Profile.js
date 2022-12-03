f1c3ec56bb24b29698dca189baa606a952e1ff90
Matt Edwards <mattsp.edwards@googlemail.com>
2022-12-03 22:07:41 +0000

    refactor: added ProfileSkeleton for loading screen

--- multilink/src/pages/Profile.js
+++ multilink/src/pages/Profile.js
@@ -3,13 +3,14 @@ import { Box } from "@mui/material";
 import ProfileBio from "../components/ProfileBio";
 import Links from "../components/Links";
 import useGetProfile from "../hooks/useGetProfile";
+import ProfileSkeleton from "../skeletons/ProfileSkeleton";
 
 export default function Profile() {
     const { profile, isLoading } = useGetProfile(
         localStorage.getItem("username")
     );
     return isLoading ? (
-        <div>Loading..</div>
+        <ProfileSkeleton />
     ) : (
         <Box my={3}>
             <ProfileBio profile={profile} editable={true} />
--- multilink/src/pages/UserProfile.js
+++ multilink/src/pages/UserProfile.js
@@ -4,13 +4,14 @@ import ProfileBio from "../components/ProfileBio";
 import Links from "../components/Links";
 import useGetProfile from "../hooks/useGetProfile";
 import { useParams } from "react-router-dom";
+import ProfileSkeleton from "../skeletons/ProfileSkeleton";
 
 export default function UserProfile() {
     const { username } = useParams();
 
     const { profile, isLoading } = useGetProfile(username);
     return isLoading ? (
-        <div>Loading..</div>
+        <ProfileSkeleton />
     ) : (
         <Box my={3}>
             <ProfileBio profile={profile} editable={false} />
